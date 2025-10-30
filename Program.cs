bd7ffd4e5befa9e9800a77aa9a10f507b8355a1b
Ioannis Batenkof <Ioannis.Batenkof@plan-b-gmbh.com>
2025-10-30 15:45:13 +0100

    different code

--- Program.cs
+++ Program.cs
@@ -4,43 +4,32 @@ using System.Threading;
 
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
         int sensor1Pin = 22; // physical pin 15
         int sensor2Pin = 26; // physical pin 37
 
-        using var gpio = new GpioController();
-        gpio.OpenPin(sensor1Pin, PinMode.InputPullUp);
-        gpio.OpenPin(sensor2Pin, PinMode.InputPullUp);
+        const int Pin = 22;
+        const string Alert = "ALERT 🚨";
+        const string Ready = "READY ✅";
 
-        Console.WriteLine("Reading sensors (GPIO22 & GPIO26). Press Ctrl+C to stop.");
+        using var controller = new GpioController();
+        controller.OpenPin(Pin, PinMode.InputPullUp);
 
-        gpio.RegisterCallbackForPinValueChangedEvent(
-            sensor1Pin, PinEventTypes.Falling | PinEventTypes.Rising, OnPin1Event);
+        Console.WriteLine(
+            $"Initial status ({DateTime.Now}): {(controller.Read(Pin) == PinValue.High ? Alert : Ready)}");
 
-        gpio.RegisterCallbackForPinValueChangedEvent(
-            sensor2Pin, PinEventTypes.Falling | PinEventTypes.Rising, OnPin2Event);
+        controller.RegisterCallbackForPinValueChangedEvent(
+            Pin,
+            PinEventTypes.Falling | PinEventTypes.Rising,
+            OnPinEvent);
 
-        static void OnPin1Event(object sender, PinValueChangedEventArgs args)
+        await Task.Delay(Timeout.Infinite);
+
+        static void OnPinEvent(object sender, PinValueChangedEventArgs args)
         {
             Console.WriteLine(
-                $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? "Low on 1" : "High on 1")}");
-        }
-
-        static void OnPin2Event(object sender, PinValueChangedEventArgs args)
-        {
-            Console.WriteLine(
-                $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? "Low on 2" : "High on 2")}");
-        }
-
-        Task.Delay(Timeout.Infinite);
-
-        while (true)
-        {
-            bool s1 = gpio.Read(sensor1Pin) == PinValue.High;
-            bool s2 = gpio.Read(sensor2Pin) == PinValue.High;
-            Console.WriteLine($"Sensor1: {s1}, Sensor2: {s2}");
-            Thread.Sleep(300);
+                $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? Alert : Ready)}");
         }
     }
 }
