f2bf019b185d55064c73bd262bf4e6eb0448613b
Ioannis Batenkof <Ioannis.Batenkof@plan-b-gmbh.com>
2025-10-30 15:36:18 +0100

    test

--- Program.cs
+++ Program.cs
@@ -9,11 +9,29 @@ class Program
         int sensor1Pin = 22; // physical pin 15
         int sensor2Pin = 26; // physical pin 37
 
-        using var gpio = new GpioController(PinNumberingScheme.Logical);
+        using var gpio = new GpioController();
         gpio.OpenPin(sensor1Pin, PinMode.InputPullUp);
         gpio.OpenPin(sensor2Pin, PinMode.InputPullUp);
 
         Console.WriteLine("Reading sensors (GPIO22 & GPIO26). Press Ctrl+C to stop.");
+
+        gpio.RegisterCallbackForPinValueChangedEvent(
+            sensor1Pin, PinEventTypes.Falling | PinEventTypes.Rising, OnPin1Event);
+
+        gpio.RegisterCallbackForPinValueChangedEvent(
+            sensor2Pin, PinEventTypes.Falling | PinEventTypes.Rising, OnPin2Event);
+
+        static void OnPin1Event(object sender, PinValueChangedEventArgs args)
+        {
+            Console.WriteLine(
+                $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? "Low on 1" : "High on 1")}");
+        }
+
+        static void OnPin2Event(object sender, PinValueChangedEventArgs args)
+        {
+            Console.WriteLine(
+                $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? "Low on 2" : "High on 2")}");
+        }
 
         while (true)
         {
