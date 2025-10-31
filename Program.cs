228250e60359ff2ca29d77db5861f7724497b24d
Ioannis Batenkof <Ioannis.Batenkof@plan-b-gmbh.com>
2025-10-31 10:08:04 +0100

    pin mode

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@ namespace Freedom.Workspace
     {
         static void Main()
         {
-            int sensor1Pin = 15;   // black wire of sensor 1
-            int sensor2Pin = 37;   // black wire of sensor 2 (OK, but consider changing to 17 or 27 later)
+            int sensor1Pin = 22;   // black wire of sensor 1
+            int sensor2Pin = 26;   // black wire of sensor 2 (OK, but consider changing to 17 or 27 later)
 
             var driver = new LibGpiodDriver(4);
             using var controller = new GpioController(driver);
 
-            controller.OpenPin(sensor1Pin, PinMode.Input);
-            controller.OpenPin(sensor2Pin, PinMode.Input);
+            controller.OpenPin(sensor1Pin, PinMode.Output);
+            controller.OpenPin(sensor2Pin, PinMode.Output);
 
             Console.WriteLine("Reading sensors... Press Ctrl+C to stop.");
 
